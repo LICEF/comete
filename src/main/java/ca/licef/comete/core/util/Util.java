0b64047cc13fdc1faa55dcfc419fff9672e00c24
Frederic Bergeron <frederic.bergeron@licef.ca>
2015-02-05 10:31:47 +0900

    Reimplemented filtering of invalid records.

--- src/main/java/ca/licef/comete/core/util/Util.java
+++ src/main/java/ca/licef/comete/core/util/Util.java
@@ -221,26 +221,29 @@ public class Util {
             throw( new Exception( "Unknown application profile: " + profileUri + "." ) );
     }
 
-    public static String getReportLink( String storeId, String profileUri ) throws Exception {
-        String urlStart = Core.getInstance().getCometeUrl() + "/rest/metadataRecords" + storeId + "/validationReport/";
-        String urlEnd = "/xml?syntaxHighlighted=true";
-        String urlMiddle = null;
+    public static String getApplProfAbbreviation( String profileUri ) throws Exception {
         if( Constants.APPL_PROF_LOM_STRICT.equals( profileUri ) )
-            urlMiddle = Constants.APPL_PROF_ABBR_LOM_STRICT;
+            return( Constants.APPL_PROF_ABBR_LOM_STRICT );
         else if( Constants.APPL_PROF_LOM_LOOSE.equals( profileUri ) )
-            urlMiddle = Constants.APPL_PROF_ABBR_LOM_LOOSE;
+            return( Constants.APPL_PROF_ABBR_LOM_LOOSE );
         else if( Constants.APPL_PROF_LOM_FR_1_0.equals( profileUri ) )
-            urlMiddle = Constants.APPL_PROF_ABBR_LOM_FR_1_0;
+            return( Constants.APPL_PROF_ABBR_LOM_FR_1_0 );
         else if( Constants.APPL_PROF_SCO_LOM_FR_1_0.equals( profileUri ) )
-            urlMiddle = Constants.APPL_PROF_ABBR_SCO_LOM_FR_1_0;
+            return( Constants.APPL_PROF_ABBR_SCO_LOM_FR_1_0 );
         else if( Constants.APPL_PROF_SCO_LOM_FR_1_1.equals( profileUri ) )
-            urlMiddle = Constants.APPL_PROF_ABBR_SCO_LOM_FR_1_1;
+            return( Constants.APPL_PROF_ABBR_SCO_LOM_FR_1_1 );
         else if( Constants.APPL_PROF_LOM_NORMETIC_1_2.equals( profileUri ) )
-            urlMiddle = Constants.APPL_PROF_ABBR_LOM_NORMETIC_1_2;
+            return( Constants.APPL_PROF_ABBR_LOM_NORMETIC_1_2 );
         else if( Constants.APPL_PROF_OAI_DC.equals( profileUri ) )
-            urlMiddle = Constants.APPL_PROF_ABBR_OAI_DC;
+            return( Constants.APPL_PROF_ABBR_OAI_DC );
         else
             throw( new Exception( "Unknown application profile: " + profileUri + "." ) );
+    }
+
+    public static String getReportLink( String storeId, String profileUri ) throws Exception {
+        String urlStart = Core.getInstance().getCometeUrl() + "/rest/metadataRecords" + storeId + "/validationReport/";
+        String urlMiddle = getApplProfAbbreviation( profileUri );
+        String urlEnd = "/xml?syntaxHighlighted=true";
         return( urlStart + urlMiddle + urlEnd );
     }
 
