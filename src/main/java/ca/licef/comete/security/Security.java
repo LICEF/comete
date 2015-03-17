8ceb00911177e999d8b165c8cb3caba50574e09f
Alexis Miara <alexis.miara@licef.ca>
2015-03-17 14:11:09 -0400

    Security IPs list in comete_data conf folder

--- src/main/java/ca/licef/comete/security/Security.java
+++ src/main/java/ca/licef/comete/security/Security.java
@@ -1,8 +1,13 @@
 package ca.licef.comete.security;
 
 
+import ca.licef.comete.core.Core;
 import licef.IOUtil;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.util.Enumeration;
 import java.util.Vector;
@@ -14,12 +19,36 @@ import java.util.Vector;
  */
 public class Security {
 
+    static File settingDir = new File(Core.getInstance().getCometeHome(), "/conf/security");
+    static File authorized = new File(settingDir, "authorized.ini");
+
     private static Security instance;
 
     public static Security getInstance() {
         if (instance == null)
             instance = new Security();
         return (instance);
+    }
+
+    public Security() {
+        try {
+            init();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void init() throws Exception {
+        if (!settingDir.exists())
+            IOUtil.createDirectory(settingDir.getAbsolutePath());
+
+        if (!authorized.exists()) {
+            InputStream is = getClass().getResourceAsStream("/conf/security/sample-authorized.ini");
+            OutputStream os = new FileOutputStream(authorized);
+            IOUtil.copy(is, os);
+            is.close();
+            os.close();
+        }
     }
 
     public boolean isAuthorized(String ip) throws Exception {
@@ -31,7 +60,7 @@ public class Security {
         //check on the trusted list
         Vector v;
         try {
-            v = IOUtil.readLines(this.getClass().getResourceAsStream("/conf/authorized.ini"));
+            v = IOUtil.readLines(authorized);
         } catch (Exception e) {
             return false;
         }
