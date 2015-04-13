0f60f7fd208493c7d9dc9da548158e9048be7ccb
Frederic Bergeron <frederic.bergeron@licef.ca>
2015-04-13 22:32:40 +0900

    Added admin's password file.

--- src/main/java/ca/licef/comete/security/resource/SecurityResource.java
+++ src/main/java/ca/licef/comete/security/resource/SecurityResource.java
@@ -2,6 +2,12 @@ package ca.licef.comete.security.resource;
 
 import ca.licef.comete.security.Security;
 import com.sun.jersey.spi.resource.Singleton;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import licef.IOUtil;
+import licef.Sha1Util;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -28,12 +34,42 @@ public class SecurityResource {
     @Path( "authentication" )
     @Produces( MediaType.TEXT_PLAIN )
     public Response authenticate( @Context HttpServletRequest req, @QueryParam( "password" ) String password ) {
-        if( !"toto".equals( password ) )
+        String sha1 = getAdminPasswordSha1();
+        try {
+            String hashedPassword = Sha1Util.hash( password );
+            if( !hashedPassword.equals( sha1 ) )
+                return( Response.ok( "false" ).build() );
+        }
+        catch( NoSuchAlgorithmException shouldNeverHappen ) {
             return( Response.ok( "false" ).build() );
+        }
 
         HttpSession session = req.getSession( true );
         session.setAttribute( "login", "admin" );
         return( Response.ok( "true" ).build() );
+    }
+
+    private String getAdminPasswordSha1() {
+        String sha1 = null;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader( new InputStreamReader( getClass().getResourceAsStream( "/conf/adminPassword.txt" ) ) );
+            sha1 = reader.readLine();
+        }
+        catch( IOException e ) {
+            e.printStackTrace();
+        }
+        finally {
+            if( reader != null ) {
+                try {
+                    reader.close();
+                }
+                catch( IOException e ) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return( sha1 );
     }
 
 }
