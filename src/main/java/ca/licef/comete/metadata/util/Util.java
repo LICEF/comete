b4e0972eede4133e41b94ce7d3354b764dd896a4
Alexis Miara <alexis.miara@licef.ca>
2015-09-16 14:53:59 -0400

    Cumulate metadatas of imported records(harvest, upload) that describes the same learning object.

--- src/main/java/ca/licef/comete/metadata/util/Util.java
+++ src/main/java/ca/licef/comete/metadata/util/Util.java
@@ -56,4 +56,48 @@ public class Util {
 
         return new String[] {errorMessage, content, namespace, pseudoOaiID};
     }
+
+    public static String[] getResourceIdentifier(String content) throws Exception {
+        return getResourceIdentifier(XMLUtil.getXMLNode(content));
+    }
+
+    public static String[] getResourceIdentifier(File file) throws Exception {
+        return getResourceIdentifier(XMLUtil.getXMLNode(file));
+    }
+
+    public static String[] getResourceIdentifier(Node node) throws Exception {
+        String errorMessage = null;
+        String content = null;
+        String identifier = null;
+        String xpath = null;
+        try {
+            content = XMLUtil.getXMLString( node );
+            String rootname = XMLUtil.getRootTagName(content);
+            Hashtable namespaces = XMLUtil.getAttributes(content, "/");
+            String[] array = StringUtil.split(rootname, ':');
+            rootname = array[array.length - 1].toLowerCase();
+            if ("lom".equals(rootname) && namespaces.containsValue(Constants.IEEE_LOM_NAMESPACE)) {
+                xpath = "//lom:lom/lom:general/lom:identifier/lom:entry";
+            }
+            else if ("dc".equals(rootname) && namespaces.containsValue(Constants.OAI_DC_NAMESPACE)) {
+                xpath = "//dc:identifier";
+            }
+            else
+                errorMessage = "Wrong metadata format.";
+
+            //check of 1.1 general identifier existence for LOM or dc:identifier for DC.
+            NodeList identifierNodes = XMLUtil.getNodeList( node, xpath );
+            for( int i = 0; i < identifierNodes.getLength(); i++ ) {
+                Node identNode = identifierNodes.item( i );
+                identifier = identNode.getTextContent();
+                break;
+            }
+            if (identifier == null)
+                errorMessage = "No identifier field.";
+        } catch (Exception e) {
+            errorMessage = "Error on record parsing.";
+        }
+
+        return new String[] {errorMessage, identifier};
+    }
 }
